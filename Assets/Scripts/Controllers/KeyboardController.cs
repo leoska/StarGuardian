2bae6e0a4fe0e7c6e092209f7f2f39c9bb09e64c
leoska <leo77551@gmail.com>
2020-09-28 22:18:49 +0300

    first commit

---
+++ Assets/Scripts/Controllers/KeyboardController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+
+public class KeyboardController : MonoBehaviour
+{
+
+    public Player Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = Player == null ? GetComponent<Player>() : Player;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool moveUp = Input.GetKey(KeyCode.W);
+        bool moveDown = Input.GetKey(KeyCode.S);
+
+        if (Player != null)
+        {
+
+            if (moveUp || moveDown)
+            {
+                float xPosition = 0f;
+                float yPosition = -Convert.ToInt32(moveDown) + Convert.ToInt32(moveUp);
+                Vector3 Directional = new Vector3(xPosition, yPosition, 0);
+
+                Player.PlayerMove(Directional);
+            }
+        }
+        else
+        {
+            Player = FindObjectOfType<Player>();
+        }
+    }
+
+    enum DirectionState
+    {
+        Up,
+        Down,
+        None,
+    }
+
+    enum MoveState
+    {
+        Idle,
+        Walk,
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Speeds")]
+    public float WalkSpeed = 3;
+    public float WalkCooldown = 0.005f;
+
+    private float _walkTime = 0;
+    private Rigidbody2D _rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _walkTime -= Time.deltaTime;
+
+        if (_walkTime <= 0)
+        {
+            _rigidbody2D.velocity = new Vector2(0f, 0f);
+            _walkTime = 0;
+        }
+    }
+
+    public void PlayerMove(Vector3 Directional)
+    {
+        if (Directional.magnitude > 0)
+        {
+            Directional.Normalize();
+            _walkTime = WalkCooldown;
+
+            if (Directional.magnitude != 0)
+            {
+                _rigidbody2D.velocity = Directional * WalkSpeed;
+            }
+        }
+        else
+        {
+            _rigidbody2D.velocity = new Vector2(0f, 0f);
+        }
+    }
+
+    private void UpMove()
+    {
+
+    }
+
+    private void DownMove()
+    {
+
+    }
+}
