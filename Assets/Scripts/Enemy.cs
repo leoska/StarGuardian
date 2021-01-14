f6651740afbaf27d50003b4ee6da2d09ab109b98
Eugene <evgeniysit005@gmail.com>
2021-01-14 13:15:04 +0300

    WIP

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,10 +8,13 @@ public class Enemy : MonoBehaviour
     public GameObject EnemyLaser;
     public float speed = 2f;
     public float bounx_x = -11f;
+    public Vector2 y_limits = new Vector2(-4.5f, 4.5f);
+    private VerticalDirection _dir;
     // Start is called before the first frame update
     void Start()
     {
         Invoke("Shoot", 2f);
+        _dir = (VerticalDirection) Random.Range(0, 2);
     }
 
     // Update is called once per frame
@@ -19,10 +22,22 @@ public class Enemy : MonoBehaviour
     {
         Vector3 pos = transform.position;
         pos.x -= speed * Time.deltaTime;
+        pos.y += _dir == VerticalDirection.Down ? -speed * Time.deltaTime : speed * Time.deltaTime;
+        if (pos.y < y_limits.x)
+        {
+            _dir = VerticalDirection.Up;
+            pos.y = y_limits.x;
 
+        }
+        else if (pos.y > y_limits.y)
+        {
+            _dir = VerticalDirection.Down;
+            pos.y = y_limits.y;
+        }
         transform.position = pos;
         if (pos.x < bounx_x)
             Destroy(gameObject);
+
     }
     void Shoot()
     {
@@ -31,5 +46,11 @@ public class Enemy : MonoBehaviour
         Instantiate<GameObject>(EnemyLaser, new Vector3(transform.position.x - 0.8f, transform.position.y, 0), rotation);
         float timer = Random.Range(timer_limits.x, timer_limits.y);
         Invoke("Shoot", timer);
+    }
+
+    public enum VerticalDirection
+    {
+        Up = 0,
+        Down = 1,
     }
 }
