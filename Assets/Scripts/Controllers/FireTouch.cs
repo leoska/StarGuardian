dbd9717aa54203fb6ba4bc931aa0c2a98f898ac9
Eugene <evgeniysit005@gmail.com>
2021-01-19 22:36:51 +0300

    WIP

---
+++ Assets/Scripts/Controllers/FireTouch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTouch : MonoBehaviour
+{
+    public Player Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = Player == null ? GetComponent<Player>() : Player;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Player.PlayerAttack();
+    }
+}
