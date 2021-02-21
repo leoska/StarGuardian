b11fd5ac0604dde6cb835e63cad84dfa1a252459
leoska <leo77551@gmail.com>
2021-02-21 23:16:45 +0300

    WIP

--- Assets/Scripts/Enemy/EnemySmartMove.cs
+++ Assets/Scripts/Enemy/EnemySmartMove.cs
@@ -1,23 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
-public class EnemyRandomMove : MonoBehaviour
+public class EnemySmartMove : MonoBehaviour
 {
     public Vector2 timer_limits = new Vector2 (0.2f, 1f);
     public GameObject EnemyLaser;
     public float speed = 2f;
     public float bounx_x = -11f;
     public Vector2 y_limits = new Vector2(-4.5f, 4.5f);
-    public Vector2 searchRect= new Vector2 ();
+    public float widthSearchRect;
 
     private static GUIStyle _gui;
     private VerticalDirection _dir;
+    private BoxCollider2D _boxCollider2d;
     // Start is called before the first frame update
     void Start()
     {
         //Invoke("Shoot", 2f);
         _dir = (VerticalDirection) Random.Range(0, 2);
+        _boxCollider2d = GetComponent<BoxCollider2D>();
         DetectionLasers();
     }
 
@@ -57,9 +60,24 @@ public class EnemyRandomMove : MonoBehaviour
 
     }
 
-    public void OnGUI()
+    private void OnDrawGizmos()
     {
-        GUI.DrawTexture(new Rect(10, 10, 10, 10), Texture2D.whiteTexture);
+        if (!_boxCollider2d)
+            _boxCollider2d = GetComponent<BoxCollider2D>();
+
+        Gizmos.color = Color.yellow;
+
+        // Calculate a 4 points
+        Vector3 x1y1 = new Vector3(transform.position.x - widthSearchRect, transform.position.y + _boxCollider2d.size.y / 2, transform.position.z);
+        Vector3 x1y2 = new Vector3(transform.position.x - widthSearchRect, transform.position.y - _boxCollider2d.size.y / 2, transform.position.z);
+        Vector3 x2y1 = new Vector3(transform.position.x, transform.position.y + _boxCollider2d.size.y / 2, transform.position.z);
+        Vector3 x2y2 = new Vector3(transform.position.x, transform.position.y - _boxCollider2d.size.y / 2, transform.position.z);
+
+        // Draw a rectangle
+        Gizmos.DrawLine(x1y1, x2y1);
+        Gizmos.DrawLine(x2y1, x2y2);
+        Gizmos.DrawLine(x2y2, x1y2);
+        Gizmos.DrawLine(x1y2, x1y1);
     }
 
     public enum VerticalDirection
